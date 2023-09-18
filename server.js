33b562196ecc16cca0032c9b36e6b88f6373d4ba
Yaswanth Varma Jampana <yjampana@uncc.edu>
2023-09-17 21:18:23 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -4,44 +4,7 @@ const port = 3000;
 
 app.use('/', express.static('Public'));
 
-const budget = {
-    myBudget: [
-
-    {
-        title: 'Eat Out',
-        budget: 30
-    },
-    {
-        title: 'Rent',
-        budget: 350
-    },
-    {
-        title: 'Groceries',
-        budget: 90
-    },
-    {
-        title: 'Shopping',
-        budget: 100
-    },
-    {
-        title: 'Electricity',
-        budget: 60
-    },
-    {
-        title: 'Water Bill',
-        budget: 40
-    },
-    {
-        title: 'Trip',
-        budget: 170
-    },
-    {
-        title: 'Others',
-        budget: 79
-    }
-    
-]
-};
+const budget = require('./mybudget.json');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
